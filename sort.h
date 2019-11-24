2d8f3196ffca434fcc668f4f47e5fef4b3141b73
BABTUL <50399467+BABTUL@users.noreply.github.com>
2019-11-24 17:46:28 +0900

    Update sort.h

--- sort.h
+++ sort.h
@@ -18,7 +18,7 @@ struct Arr< X > {
 
 template< typename T1, typename T2 >
 struct Add {
-    using Result = Add< typename T1::template Push_back< T2::head >, typename T2::Tail >::Result;
+    using Result = typename Add< typename T1::template Push_back< T2::head >, typename T2::Tail >::Result;
 };
 
 template<>
@@ -43,12 +43,12 @@ struct Filter {
 
 template< template< int > typename F, typename T >
 struct Filter< F, T, typename std::enable_if_t< !std::is_same< T, int >::value && F< T::head >::value > > {
-    using Result = Add< Arr< T::head >, typename Filter< F, typename T::Tail >::Result >::Result;
+    using Result = typename Add< Arr< T::head >, typename Filter< F, typename T::Tail >::Result >::Result;
 };
 
 template< template< int > typename F, typename T >
 struct Filter< F, T, typename std::enable_if_t< !std::is_same< T, int >::value && !F< T::head >::value > > {
-    using Result = Filter< F, typename T::Tail >::Result;
+    using Result = typename Filter< F, typename T::Tail >::Result;
 };
 
 template< int X >
@@ -73,15 +73,15 @@ template< typename T >
 struct Sort {
     template< int X >
     using Lc = typename Cmp< T::head >::template Less   < X >;
-    using Ls = Sort< typename Filter< Lc, T >::Result >::Result;
+    using Ls = typename Sort< typename Filter< Lc, T >::Result >::Result;
 
     template< int X >
     using Ec = typename Cmp< T::head >::template Equal  < X >;
-    using Es = Filter< Ec, T >::Result;
+    using Es = typename Filter< Ec, T >::Result;
 
     template< int X >
     using Gc = typename Cmp< T::head >::template Greater< X >;
-    using Gs = Sort< typename Filter< Gc, T >::Result >::Result;
+    using Gs = typename Sort< typename Filter< Gc, T >::Result >::Result;
 
     using Result = typename Add< typename Add< Ls, Es >::Result, Gs >::Result;
 };
